7d03b4aa5fed0abbcaf2b138adfcf25ae2aa71b3
Elminsterio <aljim91@gmail.com>
2022-07-06 22:32:05 +0200

    develop: refactorized legal interest calculator module

--- modules/legalInterestsCalculator.js
+++ modules/legalInterestsCalculator.js
@@ -1,40 +1,44 @@
 const dayjs = require('dayjs');
+const isBetween = require('dayjs/plugin/isBetween')
 const isLeapYear = require('dayjs/plugin/isLeapYear');
+const arraySupport = require("dayjs/plugin/arraySupport");
 
 dayjs.extend(isLeapYear);
+dayjs.extend(isBetween);
+dayjs.extend(arraySupport);
 
 const INTERESTS_RATE_PER_DATE = [
-    {startInterestsDate: '1993',  annualInterestRate: 10.0},
-    {startInterestsDate: '1994',  annualInterestRate: 9.00},
-    {startInterestsDate: '1995',  annualInterestRate: 9.00},
-    {startInterestsDate: '1996',  annualInterestRate: 9.00},
-    {startInterestsDate: '1997',  annualInterestRate: 7.50},
-    {startInterestsDate: '1998',  annualInterestRate: 5.50},
-    {startInterestsDate: '1999',  annualInterestRate: 4.25},
-    {startInterestsDate: '2000',  annualInterestRate: 4.25},
-    {startInterestsDate: '2001',  annualInterestRate: 5.50},
-    {startInterestsDate: '2002',  annualInterestRate: 4.25},
-    {startInterestsDate: '2003',  annualInterestRate: 4.25},
-    {startInterestsDate: '2004',  annualInterestRate: 3.75},
-    {startInterestsDate: '2005',  annualInterestRate: 4.00},
-    {startInterestsDate: '2006',  annualInterestRate: 4.00},
-    {startInterestsDate: '2007',  annualInterestRate: 5.00},
-    {startInterestsDate: '2008',  annualInterestRate: 5.50},
-    {startInterestsDate: '2009',  annualInterestRate: 5.50},
-    {startInterestsDate: ['2009', '4'], annualInterestRate: 4.00},
-    {startInterestsDate: '2010',  annualInterestRate: 4.00},
-    {startInterestsDate: '2011',  annualInterestRate: 4.00},
-    {startInterestsDate: '2012',  annualInterestRate: 4.00},
-    {startInterestsDate: '2013',  annualInterestRate: 4.00},
-    {startInterestsDate: '2014',  annualInterestRate: 4.00},
-    {startInterestsDate: '2015',  annualInterestRate: 3.50},
-    {startInterestsDate: '2016',  annualInterestRate: 3.00},
-    {startInterestsDate: '2017',  annualInterestRate: 3.00},
-    {startInterestsDate: '2018',  annualInterestRate: 3.00},
-    {startInterestsDate: '2019',  annualInterestRate: 3.00},
-    {startInterestsDate: '2020',  annualInterestRate: 3.00},
-    {startInterestsDate: '2021',  annualInterestRate: 3.00},
-    {startInterestsDate: '2022',  annualInterestRate: 3.00}
+    {startInterestsDate: '1993', endInterestsDate: ['1993', '11', '31'],  annualInterestRate: 10.0},
+    {startInterestsDate: '1994', endInterestsDate: ['1994', '11', '31'],  annualInterestRate: 9.00},
+    {startInterestsDate: '1995', endInterestsDate: ['1995', '11', '31'],  annualInterestRate: 9.00},
+    {startInterestsDate: '1996', endInterestsDate: ['1996', '11', '31'],  annualInterestRate: 9.00},
+    {startInterestsDate: '1997', endInterestsDate: ['1997', '11', '31'],  annualInterestRate: 7.50},
+    {startInterestsDate: '1998', endInterestsDate: ['1998', '11', '31'],  annualInterestRate: 5.50},
+    {startInterestsDate: '1999', endInterestsDate: ['1999', '11', '31'],  annualInterestRate: 4.25},
+    {startInterestsDate: '2000', endInterestsDate: ['2000', '11', '31'],  annualInterestRate: 4.25},
+    {startInterestsDate: '2001', endInterestsDate: ['2001', '11', '31'],  annualInterestRate: 5.50},
+    {startInterestsDate: '2002', endInterestsDate: ['2002', '11', '31'],  annualInterestRate: 4.25},
+    {startInterestsDate: '2003', endInterestsDate: ['2003', '11', '31'],  annualInterestRate: 4.25},
+    {startInterestsDate: '2004', endInterestsDate: ['2004', '11', '31'],  annualInterestRate: 3.75},
+    {startInterestsDate: '2005', endInterestsDate: ['2005', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2006', endInterestsDate: ['2006', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2007', endInterestsDate: ['2007', '11', '31'],  annualInterestRate: 5.00},
+    {startInterestsDate: '2008', endInterestsDate: ['2008', '11', '31'],  annualInterestRate: 5.50},
+    {startInterestsDate: '2009', endInterestsDate: ['2009', '2', '31'],  annualInterestRate: 5.50},
+    {startInterestsDate: ['2009', '3'], endInterestsDate: ['2009', '11', '31'], annualInterestRate: 4.00},
+    {startInterestsDate: '2010', endInterestsDate: ['2010', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2011', endInterestsDate: ['2011', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2012', endInterestsDate: ['2012', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2013', endInterestsDate: ['2013', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2014', endInterestsDate: ['2014', '11', '31'],  annualInterestRate: 4.00},
+    {startInterestsDate: '2015', endInterestsDate: ['2015', '11', '31'],  annualInterestRate: 3.50},
+    {startInterestsDate: '2016', endInterestsDate: ['2016', '11', '31'],  annualInterestRate: 3.00},
+    {startInterestsDate: '2017', endInterestsDate: ['2017', '11', '31'],  annualInterestRate: 3.00},
+    {startInterestsDate: '2018', endInterestsDate: ['2018', '11', '31'],  annualInterestRate: 3.00},
+    {startInterestsDate: '2019', endInterestsDate: ['2019', '11', '31'],  annualInterestRate: 3.00},
+    {startInterestsDate: '2020', endInterestsDate: ['2020', '11', '31'],  annualInterestRate: 3.00},
+    {startInterestsDate: '2021', endInterestsDate: ['2021', '11', '31'],  annualInterestRate: 3.00},
+    {startInterestsDate: '2022', endInterestsDate: ['2022', '11', '31'],  annualInterestRate: 3.00}
 ];
 
 let interestsCalculation = ({initialDate, endDate, amount, typeInterestsRate}) => {
@@ -42,13 +46,13 @@ let interestsCalculation = ({initialDate, endDate, amount, typeInterestsRate}) =
     switch(typeInterestsRate) {
 
         case 'legal':
-
         return legalInterestsCalculation(initialDate, endDate, amount);
 
         case 'mora':
-
         return legalInterestsCalculation(initialDate, endDate, amount, 2);
 
+        case 'personalizado':
+        return 
     }
 
        
@@ -56,78 +60,55 @@ let interestsCalculation = ({initialDate, endDate, amount, typeInterestsRate}) =
 
 let legalInterestsCalculation = (initialDate, endDate, amount, differential = 0) => {
 
-    
-    let initialDateParsed = dateParserToDayJS(initialDate);
-    let endDateParsed = dateParserToDayJS(endDate);
-    
-    let allPeriodsToOperate = INTERESTS_RATE_PER_DATE.filter(el => dayjs(el.startInterestsDate).get('year') >= initialDateParsed.get('year') && 
-                                                                   dayjs(el.startInterestsDate).get('year') <= endDateParsed.get('year'));
+    let initialDateParsed = completeDateParserToDayJS(initialDate);
+    let endDateParsed = completeDateParserToDayJS(endDate);
     
     let totalInterests = 0;
-    const calculation = [];
-    
-    while(allPeriodsToOperate.length > 0) {
+    const calculations = [];
 
-        
-        let daysPerInterestsCycle;
-        let nextCycleDateParsed;
+    INTERESTS_RATE_PER_DATE.forEach(period => {
+        const periodDateParsed = dayjs(period.startInterestsDate);
+        const finalPeriodDateParsed = dayjs(period.endInterestsDate);
 
-        const { startInterestsDate: actualCycleDate, annualInterestRate } = allPeriodsToOperate[0];
-        const actualCycleDateParsed = dayjs(actualCycleDate);
+        const itsBetween = periodDateParsed.isBetween(initialDateParsed, endDateParsed) || 
+                           finalPeriodDateParsed.isBetween(initialDateParsed, endDateParsed);
+        if(!itsBetween) return;
 
-        if(allPeriodsToOperate.length > 1) {
-            const { startInterestsDate: nextCycleDate } = allPeriodsToOperate[1];  
-            nextCycleDateParsed = dayjs(nextCycleDate)
-    
-            daysPerInterestsCycle = initialDateParsed.get('year') === actualCycleDateParsed.get('year') ?
-                                    nextCycleDateParsed.get('month') !== 0 ?
-                                    {days: nextCycleDateParsed.diff(initialDateParsed, 'days'), isInitial: true, isEnd: false} :
-                                    {days: actualCycleDateParsed.add(1, 'year').diff(initialDateParsed, 'days'), isInitial: true, isEnd: false} :
-                                    {days: nextCycleDateParsed.diff(actualCycleDateParsed, 'days'), isInitial: false, isEnd: false};
+        const itsInitial = initialDateParsed.isBetween(periodDateParsed, finalPeriodDateParsed);
+        const itsLast = endDateParsed.isBetween(periodDateParsed, finalPeriodDateParsed);
 
-        } else if(allPeriodsToOperate.length === 1) {
-            daysPerInterestsCycle = initialDateParsed.get('year') === endDateParsed.get('year') &&
-                                    actualCycleDateParsed.get('month') === 0 ?
-                                    {days: endDateParsed.diff(initialDateParsed, 'days'), isInitial: true, isEnd: true} :
-                                    {days: endDateParsed.diff(actualCycleDateParsed, 'days') + 1, isInitial: false, isEnd: true};
+        let daysOnPeriod;
+        if(itsInitial && itsLast) daysOnPeriod = endDateParsed.diff(initialDateParsed, 'days');
+        else if(itsInitial) daysOnPeriod = finalPeriodDateParsed.diff(initialDateParsed, 'days');
+        else if(itsLast) daysOnPeriod = endDateParsed.diff(periodDateParsed, 'days');
+        else daysOnPeriod = finalPeriodDateParsed.diff(periodDateParsed, 'days');
+        daysOnPeriod++;
 
-        }
-
-        const totalDaysActualYear = actualCycleDateParsed.isLeapYear() ? 366 : 365;
-        const ratePerDay = ((annualInterestRate + differential)/100)/totalDaysActualYear;
-
-        const interestsCycle = (amount * ratePerDay * daysPerInterestsCycle.days);
-
+        const totalDaysActualYear = periodDateParsed.isLeapYear() ? 366 : 365;
+        const ratePerDay = ((period.annualInterestRate + differential) / 100) / totalDaysActualYear;
+        const interestsCycle = (amount * ratePerDay * daysOnPeriod);
         totalInterests += interestsCycle;
-        
-        calculation.push({actualYearInterests: interestsCycle, 
-                      daysPerInterestsCycle: daysPerInterestsCycle.days, 
-                      amount, 
-                      annualInterestRate, 
-                      startDatePeriod: daysPerInterestsCycle.isInitial ? 
-                                       initialDateParsed.format('DD/MM/YYYY') : 
-                                       actualCycleDateParsed.format('DD/MM/YYYY'), 
-                      endDatePeriod: daysPerInterestsCycle.isEnd ? 
-                                     endDateParsed.format('DD/MM/YYYY') : 
-                                     nextCycleDateParsed.subtract(1, 'days').format('DD/MM/YYYY') 
+
+        calculations.push({actualYearInterests: interestsCycle, 
+                           daysPerInterestsCycle: daysOnPeriod, 
+                           amount, 
+                           annualInterestRate: period.annualInterestRate, 
+                           startDatePeriod: itsInitial ? initialDateParsed.format('DD/MM/YYYY') : periodDateParsed.format('DD/MM/YYYY'), 
+                           endDatePeriod: itsLast ? endDateParsed.format('DD/MM/YYYY') : finalPeriodDateParsed.format('DD/MM/YYYY') 
         });
+    })
 
-        allPeriodsToOperate.splice(0, 1);
-    }
+    calculations.push({ totalInterests })
 
-    calculation.push({ totalInterests })
-
-    return calculation;
+    return calculations;
 }
 
-let dateParserToDayJS = function(date) {
+let completeDateParserToDayJS = function(date) {
 
     const [ day, month, year ] = date.split('/');
 
     return dayjs(`${month} ${day} ${year}`)
 
 }
-
-legalInterestsCalculation('01/07/2000', '01/09/2021', 5000)
 
 module.exports = interestsCalculation;
